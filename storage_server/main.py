2bb5224cb32067f7122134d469aeb7f1de36e7e8
parkjunsu3321 <jackpot3016@naver.com>
2024-11-20 16:18:49 +0900

    readme_add_storage_server

--- storage_server/main.py
+++ storage_server/main.py
@@ -19,15 +19,14 @@ app.add_middleware(
 def get_img(img_name:str):
     img_path = os.path.join(IMG_DIRECTORY, img_name)
     if os.path.exists(img_path):
-        return FileResponse(img_path)  # 이미지 파일을 응답으로 반환
+        return FileResponse(img_path)
     else:
-        return {"error": "Image not found"}  # 이미지가 없으면 에러 메시지 반환
+        return {"error": "Image not found"}
     
 @app.post("/upload")
 async def upload_img(file: UploadFile = File(...)):
     img_path = os.path.join(IMG_DIRECTORY, file.filename)
     
-    # 파일을 저장
     with open(img_path, "wb") as buffer:
         buffer.write(await file.read())
     
