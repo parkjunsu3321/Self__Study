9075e47e0fcffee9186d01007c466db03fb7a697
parkjunsu3321 <jackpot3016@naver.com>
2025-03-07 00:32:07 +0900

    25.03.07 [백준] 11724 연결 요소의 개수 문제

---
+++ 백준/11724.py
@@ -0,0 +1,25 @@
+import sys
+sys.setrecursionlimit(10**7)
+
+def DFS(connect_list:list,visit:list,i:int):
+    visit[i] = True
+    for j in connect_list[i]:
+        if not visit[j]:
+            DFS(connect_list,visit,j)
+    
+
+V, E = map(int,input().split())
+connect_list = [[] for _ in range(V)]
+for i in range(E):
+    now, next = map(int,input().split())
+    connect_list[now-1].append(next-1)
+    connect_list[next-1].append(now-1)
+
+visit = [False for _ in range(V)]
+count = 0
+for i in range():
+    if not visit[i]:
+        DFS(connect_list,visit,i)
+        count += 1
+
+print(count)
