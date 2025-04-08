4415412f5fe976bc4f17a1c62b48e223cd155228
parkjunsu3321 <jackpot3016@naver.com>
2025-04-08 22:45:21 +0900

    25.04.08 [백준] 1065 한수 문제 수정

--- 백준/python/1065.py
+++ 백준/python/1065.py
@@ -3,6 +3,11 @@ input = sys.stdin.readline
 N = int(input())
 
 if N >= 99:
-    print(N)
+    result = 99
+    for num in range(100, N + 1):
+        digits = list(map(int, str(num)))
+        if digits[1] - digits[0] == digits[2] - digits[1]:
+            result += 1
+    print(result)
 else:
-    
+    print(N)
